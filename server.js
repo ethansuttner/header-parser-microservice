03452c2d2c7e84674bd153c02c8d67faf629f795
Ethan Suttner <ethan.suttner@gmail.com>
2016-08-25 22:24:59 +0000

    Added Procfile

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+var useragent = require('express-useragent')
+app.use(useragent.express());
+app.use(express.static(__dirname + "/client/"))
+
+app.get('/', function(req, res){
+  //console.log(req.useragent)
+  console.log(req.headers)
+  var headers = req.headers
+  var output = {}
+  output["ipaddress"] = headers['x-forwarded-for']
+  if (!headers['accept-language']) {
+    output["language"] = ""
+  } else if (headers['accept-language'].indexOf(",") !== -1) {
+    output["language"] = headers['accept-language'].slice(0,headers['accept-language'].indexOf(","))
+  } else {
+    output["language"] = headers['accept-language']
+  }
+  
+  if (!headers["user-agent"]) {
+    output["software"] = ""
+  } else if (headers["user-agent"].indexOf("(") !== -1 && headers["user-agent"].indexOf(")") !== -1) {
+    output["software"] = headers["user-agent"].slice(headers["user-agent"].indexOf("(")+1,headers["user-agent"].indexOf(")"))
+  } else {
+    output["software"] = headers["user-agent"]
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(output))
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
